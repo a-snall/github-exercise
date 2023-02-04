21c17592b59807d479f3322d9343f72d779a45d8
Astrid Snäll <vaski2astrid@hotmail.com>
2023-02-04 14:46:09 -0500

    version 1.0 -red

--- github/ContentView.swift
+++ github/ContentView.swift
@@ -10,12 +10,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Image(systemName: "1.circle")
+                .resizable()
+                .scaledToFit()
+            
+                                 
+            Text("V.1.")
+                .font(.largeTitle)
+                .fontWeight(.black)
         }
         .padding()
+        .foregroundColor(.red)
     }
 }
 
