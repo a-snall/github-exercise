140e06b0b74b421ef76fcd427e31f10dc73a5b0f
Astrid Snäll <vaski2astrid@hotmail.com>
2023-02-04 14:35:09 -0500

    Initial Commit

---
+++ github/githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubApp.swift
+//  github
+//
+//  Created by Astrid Snäll on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
